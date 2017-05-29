a892a3f0dfea7ace5cab53b30f7dd36d574612e7
= <tonybamboo@gmail.com>
2017-05-29 18:45:55 +0100

    Initial Commit

---
+++ TonyGreenhalgh.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using NUnit;
+using NUnit.Framework;
+
+namespace TonyGreenhalgh.Tests
+{
+
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Test1()
+        {
+
+        }
+    }
+}
